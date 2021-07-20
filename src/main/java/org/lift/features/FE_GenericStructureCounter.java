dfb038710416594428a90c1c96f4c89fb36d843e
Torsten Zesch <torsten.zesch@gmail.com>
2021-07-20 16:10:21 +0200

    Add example for feature counter

---
+++ src/main/java/org/lift/features/FE_GenericStructureCounter.java
@@ -0,0 +1,51 @@
+package org.lift.features;
+
+import java.util.Set;
+
+import org.apache.uima.fit.descriptor.TypeCapability;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.lift.api.Feature;
+import org.lift.api.FeatureExtractor;
+import org.lift.api.FeatureType;
+import org.lift.api.LiftFeatureExtrationException;
+import org.lift.type.Structure;
+
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+
+/**
+ * Counts structures
+ */
+
+@TypeCapability(inputs = { "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token"})
+public class FE_GenericStructureCounter 
+	implements FeatureExtractor
+{
+	
+	private final String structureName;
+
+	public FE_GenericStructureCounter(String structureName) {
+		this.structureName = structureName;
+	}
+	
+	@Override
+	public Set<Feature> extract(JCas jcas) 
+			throws LiftFeatureExtrationException
+	{		
+		// TODO parameterize normalization?
+		int overallCount = JCasUtil.select(jcas, Token.class).size();
+		
+		int nrOfFeature = 0;
+    	for (Structure s : JCasUtil.select(jcas, Structure.class)) {
+    		if (s.getName().equals(structureName)) {
+    			nrOfFeature++;
+    		}
+        }
+
+		//Normalization on total count of words
+		double ratio = (double) nrOfFeature / overallCount;
+		
+		return new Feature(structureName, ratio, FeatureType.NUMERIC).asSet();
+	}
+}
+
---
+++ src/test/java/org/lift/features/FE_GenericStructureCounterTest.java
@@ -0,0 +1,50 @@
+package org.lift.features;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Set;
+
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.fit.component.NoOpAnnotator;
+import org.apache.uima.jcas.JCas;
+import org.junit.Test;
+import org.lift.api.Feature;
+import org.lift.features.util.FeatureTestUtil;
+import org.lift.type.Structure;
+
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+
+public class FE_GenericStructureCounterTest {
+
+	@Test
+	public void countFE_test()
+		throws Exception
+	{
+		
+		String name = "commaCount";
+		
+		AnalysisEngine engine = createEngine(NoOpAnnotator.class);
+
+        JCas jcas = engine.newJCas();
+        engine.process(jcas);
+        jcas.setDocumentText("test ,");
+
+        Token t1 = new Token(jcas, 0, 4);
+        t1.addToIndexes();
+        
+        Token t2 = new Token(jcas, 5, 6);
+        t2.addToIndexes();
+        
+        Structure s1 = new Structure(jcas, t2.getBegin(), t2.getEnd());
+        s1.setName(name);
+        s1.addToIndexes();
+        
+		FE_GenericStructureCounter fe = new FE_GenericStructureCounter(name);
+		
+		Set<Feature> features = fe.extract(jcas);
+        assertEquals(1, features.size());
+        FeatureTestUtil.assertFeatures(name, 0.5, features, 0.00001);
+        System.out.println(features);
+	}
+}
