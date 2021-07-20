a3f803b8ec4ed0d16b9504730b1678ce00e6cf6d
Torsten Zesch <torsten.zesch@gmail.com>
2021-07-20 09:37:42 +0200

    Fix wrong init in ListBaseAE

--- src/main/java/org/lift/structures/ListBasedAnnotator_ImplBase.java
+++ src/main/java/org/lift/structures/ListBasedAnnotator_ImplBase.java
@@ -2,9 +2,6 @@ package org.lift.structures;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,14 +15,15 @@ public abstract class ListBasedAnnotator_ImplBase
 	extends JCasAnnotator_ImplBase
 {
 
+	// we keep that parameter optional, as subclasses might implement loading of default lists
 	public static final String PARAM_LIST_FILE_PATH = "listFilePath";
     @ConfigurationParameter(name = PARAM_LIST_FILE_PATH, mandatory = false, defaultValue = "")
-    private String listFilePath;
+    protected String listFilePath;
     
     // TODO shall we have a default here?
 	public static final String PARAM_LANGUAGE = "language";
     @ConfigurationParameter(name = PARAM_LANGUAGE, mandatory = false, defaultValue = "de")
-    private String language;
+    protected String language;
     
     protected Set<String> listSet;
 
@@ -35,24 +33,15 @@ public abstract class ListBasedAnnotator_ImplBase
         super.initialize(context);
 
 		try {
-			// if list file is not set, try to load default for language
-			if (listFilePath == "" || listFilePath == null) {
-				Path path = Paths.get("src/main/resources", "connectives", "connectives" + "_" + language + ".txt");
-				System.out.println(path.toString());
-				if (Files.notExists(path)) {
-					throw new ResourceInitializationException(new Throwable("Cannot initialize annotator for " + language));
-				}
-				listFilePath = path.toString();
+			if (listFilePath != "" && listFilePath != null) {
+				listSet = readList(listFilePath);
 			}
-			
-			listSet = readList(listFilePath);
-			
 		} catch (IOException e) {
 			throw new ResourceInitializationException(e);
 		}
     }
     
-	private Set<String> readList(String listFilePath) throws IOException {
+	protected Set<String> readList(String listFilePath) throws IOException {
 		
 		Set<String> listSet = new HashSet<String>();
 		for (String line : FileUtils.readLines(new File(listFilePath), "UTF-8")) {
--- src/main/java/org/lift/structures/SE_Connectives.java
+++ src/main/java/org/lift/structures/SE_Connectives.java
@@ -1,32 +1,80 @@
 package org.lift.structures;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.descriptor.TypeCapability;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
 import org.lift.type.Structure;
 
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Lemma;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 
 /**
- * counts the appearance of the specified connectives
+ * annotates where the connectives specified by the provided list appear in the document
  */
-@TypeCapability(inputs = { "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token"})
+@TypeCapability(inputs = {"de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token"})
 public class SE_Connectives
 	extends ListBasedAnnotator_ImplBase
 {
 
+	public static final String PARAM_USE_LEMMAS = "useLemmas";
+    @ConfigurationParameter(name = PARAM_USE_LEMMAS, mandatory = true, defaultValue = "false")
+    private boolean useLemmas;
+    
+    private final String NAME = "Connectives";
+    
+    public void initialize(UimaContext context)
+            throws ResourceInitializationException
+    {
+        super.initialize(context);
+
+		try {
+			// if list file is not set, try to load default for language
+			if (listFilePath == "" || listFilePath == null) {
+				Path path = Paths.get("src/main/resources", "connectives", "connectives" + "_" + language + ".txt");
+				if (Files.notExists(path)) {
+					throw new ResourceInitializationException(new Throwable("Cannot load list of connectives for language: " + language));
+				}
+				listFilePath = path.toString();
+			}
+			
+			listSet = readList(listFilePath);
+			
+		} catch (IOException e) {
+			throw new ResourceInitializationException(e);
+		}
+    }
+    
 	@Override
 	public void process(JCas jcas) 
 			throws AnalysisEngineProcessException
 	{
-		// TOOD are there languages where we need to use lemmas?
-		for (Token token : JCasUtil.select(jcas, Token.class)) {
-			if (listSet.contains(token.getCoveredText())) {
-				Structure s = new Structure(jcas, token.getBegin(), token.getEnd());
-				s.setName("Connectives");
-				s.addToIndexes();
-			}
+
+		if (useLemmas) {
+			for (Lemma lemma : JCasUtil.select(jcas, Lemma.class)) {
+				if (listSet.contains(lemma.getValue())) {
+					Structure s = new Structure(jcas, lemma.getBegin(), lemma.getEnd());
+					s.setName(NAME);
+					s.addToIndexes();
+				}
+			}	
+		}
+		else {
+			for (Token token : JCasUtil.select(jcas, Token.class)) {
+				if (listSet.contains(token.getCoveredText())) {
+					Structure s = new Structure(jcas, token.getBegin(), token.getEnd());
+					s.setName(NAME);
+					s.addToIndexes();
+				}
+			}			
 		}
 	}
 }
