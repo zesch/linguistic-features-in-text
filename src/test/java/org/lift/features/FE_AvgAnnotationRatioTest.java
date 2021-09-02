d14af4aa5879233feddf3cfdbfb81a47580302e7
marvinwest <marvin.westphal@stud.uni-due.de>
2021-09-02 15:11:15 +0200

    adds PosTokenRatio to AvgAnnotationRatioTest

--- src/test/java/org/lift/features/FE_AvgAnnotationRatioTest.java
+++ src/test/java/org/lift/features/FE_AvgAnnotationRatioTest.java
@@ -19,6 +19,8 @@ import org.junit.Test;
 import org.lift.api.Feature;
 import org.lift.features.util.FeatureTestUtil;
 
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS_NOUN;
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS_VERB;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.chunk.NC;
@@ -125,6 +127,58 @@ public class FE_AvgAnnotationRatioTest {
         String baseString = "PC_PER_SENTENCE";
         FeatureTestUtil.assertFeatures("FN_" + baseString, 1.0, features, 0.0001);
         FeatureTestUtil.assertFeatures("STANDARD_DEVIATION_OF_" + baseString, 0.0, features, 0.0001);
+	}
+	
+	@Test
+	public void avgAnnotationRatioFe_NounPOSPerToken_Test() throws Exception {
+		
+		AnalysisEngineDescription segmenter = createEngineDescription(BreakIteratorSegmenter.class);
+		AnalysisEngineDescription posTagger = createEngineDescription(OpenNlpPosTagger.class);
+		AnalysisEngineDescription chunker = createEngineDescription(OpenNlpChunker.class);
+		AnalysisEngineDescription lemmatizer = createEngineDescription(NoOpAnnotator.class);
+		AnalysisEngineDescription description = createEngineDescription(segmenter,posTagger,chunker, lemmatizer);
+		AnalysisEngine engine = createEngine(description);
+		
+		JCas jcas = engine.newJCas();
+        jcas.setDocumentLanguage("en");
+        jcas.setDocumentText("This is a test and this a nice example.");
+        engine.process(jcas);
+        
+        String dividendFeaturePath = POS_NOUN.class.getName();
+        String divisorFeaturePath = Token.class.getName();
+        FE_AvgAnnotationRatio extractor = new FE_AvgAnnotationRatio(dividendFeaturePath, divisorFeaturePath);
+        Set<Feature> features = new HashSet<Feature>(extractor.extract(jcas));
+        
+        assertEquals(2, features.size());
+        String baseString = "POS_NOUN_PER_TOKEN";
+        FeatureTestUtil.assertFeatures("FN_" + baseString, 0.2, features, 0.0001);
+        FeatureTestUtil.assertFeatures("STANDARD_DEVIATION_OF_" + baseString, 0.4, features, 0.0001);
+	}
+	
+	@Test
+	public void avgAnnotationRatioFe_VerbPOSPerToken_Test() throws Exception {
+		
+		AnalysisEngineDescription segmenter = createEngineDescription(BreakIteratorSegmenter.class);
+		AnalysisEngineDescription posTagger = createEngineDescription(OpenNlpPosTagger.class);
+		AnalysisEngineDescription chunker = createEngineDescription(OpenNlpChunker.class);
+		AnalysisEngineDescription lemmatizer = createEngineDescription(NoOpAnnotator.class);
+		AnalysisEngineDescription description = createEngineDescription(segmenter,posTagger,chunker, lemmatizer);
+		AnalysisEngine engine = createEngine(description);
+		
+		JCas jcas = engine.newJCas();
+        jcas.setDocumentLanguage("en");
+        jcas.setDocumentText("This is a test and this a nice example.");
+        engine.process(jcas);
+        
+        String dividendFeaturePath = POS_VERB.class.getName();
+        String divisorFeaturePath = Token.class.getName();
+        FE_AvgAnnotationRatio extractor = new FE_AvgAnnotationRatio(dividendFeaturePath, divisorFeaturePath);
+        Set<Feature> features = new HashSet<Feature>(extractor.extract(jcas));
+        
+        assertEquals(2, features.size());
+        String baseString = "POS_VERB_PER_TOKEN";
+        FeatureTestUtil.assertFeatures("FN_" + baseString, 0.1, features, 0.0001);
+        FeatureTestUtil.assertFeatures("STANDARD_DEVIATION_OF_" + baseString, 0.3, features, 0.0001);
 	}
 
 }
