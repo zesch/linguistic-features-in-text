ff5a7b7a43fffa6d2e826ea76d167e513ea9075a
Piush Aggarwal <aggarwalpiush@gmail.com>
2024-01-25 17:37:04 +0100

    adding spell checker functionality

--- py_lift/extractors.py
+++ py_lift/extractors.py
@@ -1,10 +1,14 @@
 from cassis import Cas
-from util import load_typesystem
+
+from util import load_typesystem as lt
+from spellchecker import SpellChecker
+
+
 
 class FEL_AnnotationRatio:
 
     def __init__(self, type_dividend, type_divisor):
-        self.ts = load_typesystem('data/TypeSystem.xml')
+        self.ts = lt('data/TypeSystem.xml')
         self.dividend_type = type_dividend
         self.divisor_type = type_divisor
 
@@ -41,3 +45,28 @@ class FE_TokensPerSentence(FEL_AnnotationRatio):
     def __init__(self):
         super().__init__('de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token',
                          'de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence')
+
+
+
+
+class SE_SpellErrorAnnotator:
+
+    def __init__(self):
+        self.ts = lt('data/TypeSystem.xml')
+        self.spell = SpellChecker()
+        T_ANNOTATION = 'de.tudarmstadt.ukp.dkpro.core.api.anomaly.type.SpellingAnomaly'
+        S_SUGGESTION = 'de.tudarmstadt.ukp.dkpro.core.api.anomaly.type.SuggestedAction'
+        self.A = self.ts.get_type(T_ANNOTATION)
+        self.S = self.ts.get_type(S_SUGGESTION)
+
+    def process(self, cas: Cas) -> bool: 
+        #for sentence in cas.select('de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence'):
+        for token in cas.select('de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token'):
+            if token.get_covered_text() in self.spell.unknown([token.get_covered_text()]):
+                annotation = self.A(begin=token.begin, end=token.end) 
+                suggested_action = self.S(replacement=self.spell.correction(token.get_covered_text()), begin=token.begin, end=token.end)
+                cas.add(annotation)
+                cas.add(suggested_action)
+
+        return True
+
