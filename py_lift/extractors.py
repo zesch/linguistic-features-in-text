0846347f21af2da9fe2e4f0dd36013f59c6ac0fe
Jeanette Bewersdorff <jeanette.bewersdorff@gmail.com>
2024-02-02 14:55:12 +0100

    rework flesch index

--- py_lift/extractors.py
+++ py_lift/extractors.py
@@ -1,6 +1,6 @@
 from cassis import Cas
 from util import load_typesystem
-from textstat import *
+
 
 class FEL_AnnotationRatio:
 
@@ -33,26 +33,7 @@ class FEL_AnnotationRatio:
 
         return True
 
-class FEL_ReadabilityScore:
-    def __init__(self, lang):
-        self.ts = load_typesystem('data/TypeSystem.xml')
-        self.lang = lang
 
-    def extract(self, cas: Cas, view) -> bool:
-        casView = cas.get_view(view)
-        textstat.set_lang(self.lang)
-        readability_score = textstat.flesch_reading_ease(casView.sofa_string)
-        #print('FLESCH_KINCAID: ' + str(readability_score))
-        name = 'Readability_Score_Flesch_Kincaid_Lang_' + self.lang
-
-        # write feature value in CAS
-        T_FEATURE = 'org.lift.type.FeatureAnnotationNumeric'
-
-        F = self.ts.get_type(T_FEATURE)
-        feature = F(name=name, value=readability_score)
-        cas.add(feature)
-
-        return True
 
 
 class FE_NounPhrasesPerSentence(FEL_AnnotationRatio):
@@ -65,7 +46,5 @@ class FE_TokensPerSentence(FEL_AnnotationRatio):
         super().__init__('de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token',
                          'de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence')
 
-class FE_FleschIndex(FEL_ReadabilityScore):
-    def __init__(self):
-        super().__init__('de')
+
 
