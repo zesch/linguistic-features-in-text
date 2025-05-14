cb8f652cd985a95f777b045863401bf80dc03431
Torsten Zesch <torsten.zesch@gmail.com>
2025-05-14 21:27:17 +0200

    Use pytest.approx for float comparison

--- py_lift/tests/test_readability.py
+++ py_lift/tests/test_readability.py
@@ -12,7 +12,7 @@ def test_readability():
     i = 0
     for feature in cas.select("org.lift.type.FeatureAnnotationNumeric"):
         assert feature.name == 'Readability_Score_Flesch_Kincaid_Lang_de'
-        assert feature.value == 76.9
+        assert pytest.approx(feature.value, 0.1) == 76.9
         i += 1
 
     assert i == 1
