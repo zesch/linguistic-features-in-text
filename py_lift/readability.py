52166e7a1cc2026786c7ffcb1cb941371b2e31b9
Torsten Zesch <torsten.zesch@gmail.com>
2025-10-08 17:48:40 +0200

    Improve structure of readability extractors a bit.

--- py_lift/readability.py
+++ py_lift/readability.py
@@ -2,27 +2,63 @@ from cassis import Cas
 from util import load_lift_typesystem
 from textstat import textstat
 from extractors import FEL_BaseExtractor
+from abc import abstractmethod
 
-class FEL_ReadabilityScore(FEL_BaseExtractor):
+# TODO expose the remaining measures for english and spanish
+class FEL_TextstatReadabilityScore(FEL_BaseExtractor):
     def __init__(self, language):
         self.ts = load_lift_typesystem()
         self.language = language
 
+    @abstractmethod
+    def score(self, text: str) -> float:
+        pass
+
+    @abstractmethod
+    def name(self) -> str:
+        pass
+
     def extract(self, cas: Cas) -> bool:
         textstat.set_lang(self.language)
-        readability_score = textstat.flesch_reading_ease(cas.sofa_string)
+        readability_score = self.score(cas.sofa_string)
 
-        name = 'Readability_Score_Flesch_Kincaid_Lang_' + self.language
+        feature_name = self.name() + '_' + self.language
 
         T_FEATURE = 'org.lift.type.FeatureAnnotationNumeric'
 
         F = self.ts.get_type(T_FEATURE)
-        feature = F(name=name, value=readability_score, begin=0, end=0)
+        feature = F(name=feature_name, value=readability_score, begin=0, end=0)
         cas.add(feature)
 
         return True
 
 
-class FE_FleschIndex(FEL_ReadabilityScore):
-    def __init__(self):
-        super().__init__('de')
+class FE_TextstatFleschIndex(FEL_TextstatReadabilityScore):
+    def __init__(self, language):
+        supported_langs = ['en', 'de', 'es', 'fr', 'it', 'nl', 'ru']
+        if language not in supported_langs:
+            raise ValueError(
+                f"{self.language} is not a supported language."
+            )
+        super().__init__(language)
+
+    def name(self):
+        return 'Readability_Score_Flesch_Kincaid'
+    
+    def score(self, text: str) -> float:
+        return textstat.flesch_reading_ease(text)
+
+class FE_TextstatGunningFog(FEL_TextstatReadabilityScore):
+    def __init__(self, language):
+        supported_langs = ['en', 'pl']
+        if language not in supported_langs:
+            raise ValueError(
+                f"{self.language} is not a supported language."
+            )
+        super().__init__(language)
+
+    def name(self):
+        return 'Readability_Score_Gunning_Fog'
+    
+    def score(self, text: str) -> float:
+        return textstat.gunning_fog(text)
--- py_lift/tests/test_util.py
+++ py_lift/tests/test_util.py
@@ -1,13 +1,13 @@
 import pytest
 from annotators import SE_SpellErrorAnnotator
 from extractors import FEL_AnnotationCounter, FEL_AnnotationRatio
-from readability import FE_FleschIndex
+from readability import FE_TextstatFleschIndex
 from lift_fixtures import *
 from util import df_features, get_all_subclasses
 
 def test_extractors(cas_en_simple_with_errors):
-    SE_SpellErrorAnnotator("en").process(cas_en_simple_with_errors)
-    FE_FleschIndex().extract(cas_en_simple_with_errors)
+    SE_SpellErrorAnnotator('en').process(cas_en_simple_with_errors)
+    FE_TextstatFleschIndex('en').extract(cas_en_simple_with_errors)
     FEL_AnnotationCounter('SpellingAnomaly').extract(cas_en_simple_with_errors)
 
     df = df_features(cas_en_simple_with_errors)
