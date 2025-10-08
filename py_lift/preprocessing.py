c87e73b914447ed28ecf6f115a0798aec9defb3f
Jeanette Bewersdorff <jeanette.bewersdorff@gmail.com>
2025-10-08 23:43:25 +0200

    sidebar and preprocessing dep and pos

--- py_lift/preprocessing.py
+++ py_lift/preprocessing.py
@@ -1,7 +1,7 @@
 import spacy
 import unicodedata
 from cassis import Cas
-from dkpro import T_TOKEN, T_SENT
+from dkpro import T_TOKEN, T_SENT, T_POS, T_DEP
 from util import load_lift_typesystem
 
 class Spacy_Preprocessor:
@@ -35,6 +35,8 @@ class Spacy_Preprocessor:
 
         T = self.ts.get_type(T_TOKEN)
         S = self.ts.get_type(T_SENT)
+        P = self.ts.get_type(T_POS)
+        D = self.ts.get_type(T_DEP)
 
         for sent in doc.sents:
             begin = doc[sent.start-1].idx
@@ -42,8 +44,20 @@ class Spacy_Preprocessor:
             cas_sentence = S(begin=begin, end=end)
             self.cas.add(cas_sentence)
 
+        token_with_annotations = []
         for token in doc:
             cas_token = T(begin=token.idx, end=token.idx+len(token.text), id=token.i)
             self.cas.add(cas_token)
+
+            cas_pos = P(begin=token.idx, end=token.idx+len(token.text), PosValue=token.pos)
+            self.cas.add(cas_pos)
+            token_with_annotations.append(cas_pos)
+
+        for tok in doc:
+            if tok.head == tok:
+                continue
+            cas_dep = D(begin=tok.idx, end=tok.idx+len(tok.text), Governor=token_with_annotations[tok.head.i],
+                        Dependent=token_with_annotations[tok.i], DependencyType=tok.dep_,flavor='basic')
+            self.cas.add(cas_dep)
 
         return self.cas
--- py_lift/workbench.py
+++ py_lift/workbench.py
@@ -26,54 +26,65 @@ cas = spacy.run(text)
 
 classes_SEL = get_all_subclasses(annotators, SEL_BaseAnnotator)
 
-name_to_class = {cls.__name__: cls for cls in classes_SEL}
-selected_class_name = st.selectbox("Choose an SE", name_to_class.keys())
-selected_SE = name_to_class[selected_class_name]
+#col_a, col_b = st.columns(2)
 
+with st.sidebar:
+#with col_a:
 
-try:
-    params = get_constructor_params(selected_SE)
+    name_to_class = {cls.__name__: cls for cls in classes_SEL}
+    selected_class_name = st.selectbox("Choose an SE", name_to_class.keys())
+    selected_SE = name_to_class[selected_class_name]
 
-    user_inputs = {}
-    for param in params:
-        if param.annotation == int:
-            user_inputs[param.name] = st.number_input(param.name, value=0)
-        elif param.annotation == float:
-            user_inputs[param.name] = st.number_input(param.name, value=0.0)
-        else:  # Default to string
-            user_inputs[param.name] = st.text_input(param.name)
-    
-    if st.button("Run SEs"):
-        obj = selected_SE(**user_inputs)
-        if not obj.process(cas):
-            st.error("Processing of {obj.__class__.__name__} failed.")
-except Exception as e:
-    st.error(f"Error: {e}")
+    try:
+        params = get_constructor_params(selected_SE)
 
-classes_readability = get_all_subclasses(readability, FEL_TextstatReadabilityScore)
-classes_counters = get_all_subclasses(extractors, FEL_AnnotationCounter)
-classes_ratios = get_all_subclasses(extractors, FEL_AnnotationRatio)
+        user_inputs = {}
+        for param in params:
+            if param.annotation == int:
+                user_inputs[param.name] = st.number_input(param.name, value=0)
+            elif param.annotation == float:
+                user_inputs[param.name] = st.number_input(param.name, value=0.0)
+            else:  # Default to string
+                user_inputs[param.name] = st.text_input(param.name)
 
-name_to_FEs = {cls.__name__: cls for cls in chain(classes_readability, classes_counters, classes_ratios)}
-selected_FE_names = st.multiselect("Choose one or more FEs", name_to_FEs.keys())
-selected_FEs = [name_to_FEs[name] for name in selected_FE_names]
-
-# run all selected FEs
-fe_params = {'language': 'de'}
-try:
-    if st.button("Run FEs"):
-        for selected_FE in selected_FEs:
-            # TODO how to configure that in streamlit, assuming language parameter for now
-            obj = selected_FE(**fe_params)
-            if not obj.extract(cas):
+        select_type = st.selectbox('Select type to highlight',
+                                   ['Abstractness', 'Frequency', 'Spelling', 'POS', 'Token'])
+        if st.button("Run SEs"):
+            obj = selected_SE(**user_inputs)
+            if not obj.process(cas):
                 st.error("Processing of {obj.__class__.__name__} failed.")
-except Exception as e:
-    st.error(f"Error: {e}")
+    except Exception as e:
+        st.error(f"Error: {e}")
+
+
+#with col_b:
+
+    classes_readability = get_all_subclasses(readability, FEL_TextstatReadabilityScore)
+    classes_counters = get_all_subclasses(extractors, FEL_AnnotationCounter)
+    classes_ratios = get_all_subclasses(extractors, FEL_AnnotationRatio)
+
+    name_to_FEs = {cls.__name__: cls for cls in chain(classes_readability, classes_counters, classes_ratios)}
+    selected_FE_names = st.multiselect("Choose one or more FEs", name_to_FEs.keys())
+    selected_FEs = [name_to_FEs[name] for name in selected_FE_names]
+
+
+    # run all selected FEs
+    #fe_params = {'language': 'de'}
+    fe_params = {}
+    try:
+        if st.button("Run FEs"):
+            for selected_FE in selected_FEs:
+                # TODO how to configure that in streamlit, assuming language parameter for now
+                obj = selected_FE(**fe_params)
+                if not obj.extract(cas):
+                    st.error("Processing of {obj.__class__.__name__} failed.")
+    except Exception as e:
+        st.error(f"Error: {e}")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    select_type = st.selectbox('Select type to highlight', ['Abstractness', 'Frequency', 'Spelling', 'POS', 'Token'])
+
     span_vis = SpanVisualizer(ts)
     span_vis.selected_span_type = SpanVisualizer.UNDERLINE
     span_vis.allow_highlight_overlap = True
